781c2307359a6c5c29170db9354b8aed4da0b15e
jemmaachodor15 <jemmaachodor@gmail.com>
2025-11-06 04:46:18 -0800

    user variable and class

--- UserClass.java
+++ UserClass.java
@@ -1,5 +1,6 @@
 public class UserClass{
-	
+	public static void main(String[] args){
+	}
 	String name;
 	int age;
 	int idcard;
--- UserVariable.java
+++ UserVariable.java
@@ -5,12 +5,21 @@ public class UserVariable{
 		UserClass thing2 = new UserClass("Theophilus",24,2);
 		UserClass thing3 = new UserClass("Somina",17,3);
 		
-		System.out.printf("Thing1.name%s%n,Thing1.age%d%n,Thing1.idcard%d%n");
-			
-		}
+		
+		System.out.println("................................................");
+		System.out.printf("Thing1 name  :%s%nThing1 age:%d%nThing1 idcard:%d%n",thing1.name,thing1.age,thing1.idcard);
+		System.out.println();
+		System.out.println("...................................................");
+		System.out.printf("Thing2 name:%s%nThing2 age:%d%nThing2 idcard:%d%n",thing2.name,thing2.age,thing2.idcard);
+		System.out.println();
+		System.out.println("...................................................");
+		System.out.printf("Thing3 name:%s%nThing3 age:%d%nThing3 idcard:%d%n",thing3.name,thing3.age,thing3.idcard);
+		
+		
+		System.out.printf("The total count is:%d",UserClass.count);
+		
+	}	
 		
 		
 		
-		
-	}
 }
