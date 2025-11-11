7f08fdafa07ae9298a9e616d29a093fe9dc8c88b
jemmaachodor15 <jemmaachodor@gmail.com>
2025-11-10 22:23:31 -0800

    Local variables

---
+++ LocalVariable.java
@@ -0,0 +1,15 @@
+public class LocalVariable{
+	static int x = 20;
+	public static void main(String[] args){
+		int num = 16;
+		myPrintMethod();
+	}
+	
+	public static void myMethod(){
+	}
+	
+	public static void myPrintMethod(){
+		System.out.println(x);
+	}
+}
+	
